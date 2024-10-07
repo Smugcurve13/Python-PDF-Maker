b9d3e43e81fa514ed6150e3e485bcf03715f7ccc
Smugcurve13 <sambhavsoni14@gmail.com>
2024-10-07 12:39:13 +0530

    added new page

--- main.py
+++ main.py
@@ -6,4 +6,8 @@ print(type(pdf))
 
 pdf.add_page()
 
+pdf.set_font(family="Times",style="b", size=12)
+pdf.cell(w=0,h=12, txt="hello there",align='L', ln=1,border=1)
+pdf.cell(w=0,h=12, txt="hi there",align='L', ln=1,border=1)
+
 pdf.output("output.pdf")
